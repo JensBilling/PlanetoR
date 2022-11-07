983da80a705754db4a80b7b0469d3258dc6b57b2
Jens B <jensis92@hotmail.com>
2022-11-07 14:43:53 +0100

    Add email field for UserDto and connected AuthController to mySql DB instead of just mem.

--- PlanetoR/Controllers/AuthController.cs
+++ PlanetoR/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PlanetoR.Data;
 using PlanetoR.Models;
 using PlanetoR.Utility;
 
@@ -8,23 +9,33 @@ namespace PlanetoR.Controllers;
 [ApiController]
 public class AuthController : ControllerBase
 {
-    public static User user = new User();
 
     private readonly IConfiguration _configuration;
+    private readonly AppDbContext _context;
 
-    public AuthController(IConfiguration configuration)
+    public AuthController(IConfiguration configuration, AppDbContext context)
     {
         _configuration = configuration;
+        _context = context;
     }
 
     [HttpPost("register")]
     public async Task<ActionResult<User>> RegisterUser(UserDto requestUserDto)
     {
         AuthHelper.CreatePasswordHash(requestUserDto.Password, out var passwordHash, out var passwordSalt);
-
+        var user = new User();
+        var apiKeyGuid = Guid.NewGuid().ToString();
+        apiKeyGuid = apiKeyGuid.Replace("-", "");
+        apiKeyGuid = apiKeyGuid.ToLower();
+        
         user.Username = requestUserDto.Username;
         user.PasswordHash = passwordHash;
         user.PasswordSalt = passwordSalt;
+        user.Email = requestUserDto.Email;
+        user.ApiKey = apiKeyGuid;
+
+        _context.users.Add(user);
+        await _context.SaveChangesAsync();
 
         return Ok("User created");
     }
@@ -32,17 +43,19 @@ public class AuthController : ControllerBase
     [HttpPost("login")]
     public async Task<ActionResult<string>> LoginUser(UserDto requestUserDto)
     {
-        if (user.Username != requestUserDto.Username)
+        var foundUser = _context.users.FirstOrDefault(u => u.Username == requestUserDto.Username);
+
+        if (foundUser == null)
         {
             return BadRequest("User not found!");
         }
 
-        if (!AuthHelper.VerifyPasswordHash(requestUserDto.Password, user.PasswordHash, user.PasswordSalt))
+        if (!AuthHelper.VerifyPasswordHash(requestUserDto.Password, foundUser.PasswordHash, foundUser.PasswordSalt))
         {
             return BadRequest("Wrong password!");
         }
 
-        string jwt = AuthHelper.CreateToken(user);
+        string jwt = AuthHelper.CreateToken(foundUser);
 
         return jwt;
     }
