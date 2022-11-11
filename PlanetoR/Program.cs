9d6400ae35a451a5cc279e0fe3b43e6e5928bd49
Jens B <jensis92@hotmail.com>
2022-11-11 13:34:41 +0100

    Finished implementing auto-emailing on the same day a rocker will launch

--- PlanetoR/Program.cs
+++ PlanetoR/Program.cs
@@ -52,7 +52,7 @@ builder.Services.AddQuartz(q =>
         options.ForJob(jobKeySatelliteAutoUpdate).WithIdentity("SatelliteAutoUpdate-trigger").WithCronSchedule("1 * * * * ?"));
     
     q.AddTrigger(options =>
-        options.ForJob(jobKeyAutoEmail).WithIdentity("SendAutoLaunchEmail-trigger").WithCronSchedule("0/5 * * * * ?"));
+        options.ForJob(jobKeyAutoEmail).WithIdentity("SendAutoLaunchEmail-trigger").WithCronSchedule("* * 1 * * ?"));
 });
 
 builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
--- PlanetoR/Utility/SendAutoLaunchEmail.cs
+++ PlanetoR/Utility/SendAutoLaunchEmail.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text.Json;
 using PlanetoR.Models;
 using Quartz;
 
@@ -12,25 +14,53 @@ public class SendAutoLaunchEmail : IJob
     {
         _httpClientFactory = httpClientFactory;
     }
-    
+
     public Task Execute(IJobExecutionContext context)
     {
         var httpClient = _httpClientFactory.CreateClient();
         var launchJsonFromApi = httpClient.GetStringAsync("https://fdo.rocketlaunch.live/json/launches/next/1").Result;
 
-        
+
         var launchDescription = ExtractDataFromJson.extractString(launchJsonFromApi, "launch_description");
         var winOpen = ExtractDataFromJson.extractString(launchJsonFromApi, "win_open");
         var launchDate = DateTime.Parse(winOpen);
-
-        Console.WriteLine("_____________");
+        
         if (launchDate.Day.Equals(DateTime.Now.Day))
         {
-            // execute "launch today"-email here
+            Console.WriteLine(MailSender("jonasgranbom@hotmail.com", launchDescription, "A rocket will launch today!"));
+            Console.WriteLine(MailSender("jensis92@hotmail.com", launchDescription, "A rocket will launch today!"));
+            Console.WriteLine(MailSender("gunnarssonminna@gmail.com", launchDescription, "A rocket will launch today! VISA LITE ENGAGEMANG FÖR HELVETE, BABY!!!"));
         }
-        
-        
-        
+
         return Task.FromResult(true);
+    }
+
+    private string MailSender(string receiver, string messageContent, string subject)
+    {
+        MailAddress to = new MailAddress(receiver);
+        MailAddress from = new MailAddress("planetor@jensbilling.se");
+        MailMessage message = new MailMessage(from, to);
+        message.Subject = subject;
+        message.Body = messageContent;
+
+        SmtpClient client = new SmtpClient("smtp.simply.com", 587)
+        {
+            Credentials = new NetworkCredential("planetor@jensbilling.se", "planetor666"),
+            EnableSsl = true
+        };
+
+        try
+        {
+            client.Send(message);
+            return "email sent to " + receiver;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            return "email not sent";
+        }
+
+
+        
     }
 }
